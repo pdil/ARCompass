f82dfbf404d90b53e1f5b91682713a3c6c3cd0ec
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2018-08-26 14:31:08 -0400

    Expose test struct initializer

--- Sources/ARCompass/ARCompass.swift
+++ Sources/ARCompass/ARCompass.swift
@@ -2,6 +2,10 @@
 public struct ARCompass {
     static var text = "Hello, World!"
     
+    public init() {
+        
+    }
+    
     public static func displayText() {
         print(text)
     }
